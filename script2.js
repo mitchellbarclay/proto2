fc0775391d6d7b9fe4fdfd45e569653947f44d55
Mitchell Barclay <mitchellbarclay2@gmail.com>
2019-04-28 20:54:22 +1200

    update

--- script2.js
+++ script2.js
@@ -32,14 +32,33 @@ function showPosition(position) {
 	var fourteen = document.getElementById("r4c2");
 	var fifteen = document.getElementById("r4c3");
 	var sixteen = document.getElementById("r4c4");
+	var marker = document.getElementById("marker");
 
-	var marker = document.getElementById("marker"); 
+
+
 
 
 
   x.innerHTML = "Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude;
 
+   xMax = 969;
+   xMin = 220;
+
+   yMax = 174.778;
+   yMin = 174.774;
+
+   xMax2 = 966;
+   xMin2 = 220;
+
+   yMax2 = -41.297
+   yMin2 = -41.294
+
+  percent = (position.coords.longitude - yMin) / (yMax - yMin);
+  marker.style.marginLeft = percent * (xMax - xMin) + xMin + "px";
+
+  percent2 = (position.coords.latitude - yMin2) / (yMax2 - yMin2);
+  marker.style.marginTop = percent2 * (xMax2 - xMin2) + xMin2 + "px";
 
 
 
@@ -47,82 +66,94 @@ function showPosition(position) {
 
 
 
-
-	marker.style.background = "red";
-
+ 
 
 
+    if (position.coords.longitude < 174.774 || position.coords.longitude > 174.778 || position.coords.latitude < -41.297 || position.coords.latitude > -41.294){
+    	x.innerHTML = "out of range"
+    	marker.style.opacity = "0";
+    }
+    if (position.coords.longitude > 174.774 || position.coords.longitude < 174.778 || position.coords.latitude > -41.297 || position.coords.latitude < -41.294){
+    	
+    	marker.style.opacity = "1";
+    }
 
-	if (position.coords.latitude < -41.294 && position.coords.latitiude > -41.29475){
 
-	 if (position.coords.longitude < 174.775 && position.coords.longitude > 174.774) {
+
+
+    if (position.coords.latitude <= -41.294 && position.coords.latitude >= -41.29475){
+
+	 if (position.coords.longitude <= 174.775 && position.coords.longitude >= 174.774) {
 		one.style.opacity = "1";
 	}
-	 if (position.coords.longitude < 174.776 && position.coords.longitude > 174.775) {
+	 if (position.coords.longitude <= 174.776 && position.coords.longitude >= 174.775) {
 		two.style.opacity = "1";
 	}
 
-	 if (position.coords.longitude < 174.777 && position.coords.longitude > 174.776) {
+	 if (position.coords.longitude <= 174.777 && position.coords.longitude >= 174.776) {
 		three.style.opacity = "1";
 	}
 
-	 if (position.coords.longitude < 174.778 && position.coords.longitude > 174.777) {
+	 if (position.coords.longitude <= 174.778 && position.coords.longitude >= 174.777) {
 		four.style.opacity = "1";
 	}
 
 	}
-	else if (position.coords.latitude < -41.29475 && position.coords.latitude > -41.2955){
 
-	 if (position.coords.longitude < 174.775 && position.coords.longitude > 174.774) {
+
+
+	if (position.coords.latitude <= -41.29475 && position.coords.latitude >= -41.2955){
+
+	 if (position.coords.longitude <= 174.775 && position.coords.longitude >= 174.774) {
 		five.style.opacity = "1";
 	}
-	 if (position.coords.longitude < 174.776 && position.coords.longitude > 174.775) {
+	 if (position.coords.longitude <= 174.776 && position.coords.longitude >= 174.775) {
 		six.style.opacity = "1";
 	}
 
-	 if (position.coords.longitude < 174.777 && position.coords.longitude > 174.776) {
+	 if (position.coords.longitude <= 174.777 && position.coords.longitude >= 174.776) {
 		seven.style.opacity = "1";
 	}
 
-	 if (position.coords.longitude < 174.778 && position.coords.longitude > 174.777) {
+	 if (position.coords.longitude <= 174.778 && position.coords.longitude >= 174.777) {
 		eight.style.opacity = "1";
 	}
 
 	}
 
-	else if (position.coords.latitude < -41.2955 && position.coords.latitude > -41.29625){
+	if (position.coords.latitude <= -41.2955 && position.coords.latitude >= -41.29625){
 
-	 if (position.coords.longitude < 174.775 && position.coords.longitude > 174.774) {
+	 if (position.coords.longitude <= 174.775 && position.coords.longitude >= 174.774) {
 		nine.style.opacity = "1";
 	}
-	 if (position.coords.longitude < 174.776 && position.coords.longitude > 174.775) {
+	 if (position.coords.longitude <= 174.776 && position.coords.longitude >= 174.775) {
 		ten.style.opacity = "1";
 	}
 
-	 if (position.coords.longitude < 174.777 && position.coords.longitude > 174.776) {
+	 if (position.coords.longitude <= 174.777 && position.coords.longitude >= 174.776) {
 		eleven.style.opacity = "1";
 	}
 
-	 if (position.coords.longitude < 174.778 && position.coords.longitude > 174.777) {
+	 if (position.coords.longitude <= 174.778 && position.coords.longitude >= 174.777) {
 		twelve.style.opacity = "1";
 	}
 
 	}
 
-	else if (position.coords.latitude < -41.29625 && position.coords.latitude > -41.297){
+	if (position.coords.latitude <= -41.29625 && position.coords.latitude >= -41.297){
 
-	 if (position.coords.longitude < 174.775 && position.coords.longitude > 174.774) {
+	 if (position.coords.longitude <= 174.775 && position.coords.longitude >= 174.774) {
 		thirteen.style.opacity = "1";
 	}
-	 if (position.coords.longitude < 174.776 && position.coords.longitude > 174.775) {
+	 if (position.coords.longitude <= 174.776 && position.coords.longitude >= 174.775) {
 		fourteen.style.opacity = "1";
 	}
 
-	 if (position.coords.longitude < 174.777 && position.coords.longitude > 174.776) {
+	 if (position.coords.longitude <= 174.777 && position.coords.longitude >= 174.776) {
 		fifteen.style.opacity = "1";
 	}
 
-	 if (position.coords.longitude < 174.778 && position.coords.longitude > 174.777) {
+	 if (position.coords.longitude <= 174.778 && position.coords.longitude >= 174.777) {
 		sixteen.style.opacity = "1";
 	}
 
